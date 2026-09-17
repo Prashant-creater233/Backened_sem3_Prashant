f2411dafb9d60fea5407db64ad23a84eb04e5b2f
Prashant-creater233 <prashantkanwal8@gmail.com>
2026-09-18 00:25:23 +0530

    Refactor server.js to improve structure and add route handling for GET and POST requests

--- Lecture11/server.js
+++ Lecture11/server.js
@@ -1,8 +1,36 @@
-const express=require("express");
-const app=express()
-const PORT = 3000;
+const express = require("express");
 
+const app = express();
 
-app.listen(PORT, ()=>{
-    console.log("server is running on port 3000");
-})
+app.use(express.json());
+
+// GET request - ID route
+app.get("/id/:id", (req, res) => {
+    // console.log(req.url);
+    // console.log(req.method);
+    // console.log(req.headers);
+
+    console.log(req.params.id);
+
+    res.send("Hello Students from id route");
+});
+
+// GET request - Query parameters
+app.get("/", (req, res) => {
+    console.log(req.query.name);
+    console.log(req.query.age);
+
+    res.send("Hello Students from / route");
+});
+
+// POST request
+app.post("/", (req, res) => {
+    console.log(req.body);
+
+    res.send("Hello Students");
+});
+
+// Start server
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
